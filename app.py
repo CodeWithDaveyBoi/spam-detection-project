ae7b3cecf09823c09f15a0fb4ed4e83860826cc0
Davey Ferns <139205465+CodeWithDaveyBoi@users.noreply.github.com>
2026-04-27 11:46:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ import streamlit as st
 import pickle
 import re
 import string
+import nltk
+
+nltk.download('stopwords')
+
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
-
 # Load model and vectorizer
 with open('../models/spam_model.pkl', 'rb') as f:
     model = pickle.load(f)
